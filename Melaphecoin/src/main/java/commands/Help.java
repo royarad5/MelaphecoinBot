96ce052150f23e5f664ee9437eb27b240bb4e9e7
Almog <whyalmog@gmail.com>
2020-12-19 20:06:25 +0200

    Changed help contents

--- Melaphecoin/src/main/java/commands/Help.java
+++ Melaphecoin/src/main/java/commands/Help.java
@@ -8,19 +8,25 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Help extends ListenerAdapter {
 
-    private static final String HELP_MESSAGE = "```?bal - will show you the balance in your account.\r\n" + "\r\n"
-	    + "?bal @person - will show you the balance of the person you tag.\r\n" + "\r\n"
-	    + "?cf *amount* @person - coinflip between you and the person you tagged for a specific amount of money.\r\n"
-	    + "\r\n" + "?ds - daily spin which will be available once every day for free.\r\n" + "\r\n"
-	    + "?ds buy - after using your daily spin you will be able to buy another daily spin for 75 coins by using this command.\r\n"
-	    + "\r\n" + "?give *amount* @person - will give the person you tag a specific amount of money.\r\n" + "\r\n"
+    private static final String HELP_MESSAGE = "```?balance - will show you the balance in your account.\r\n" + "\r\n"
+	    + "?balance @person - will show you the balance of the person you tag.\r\n" + "\r\n"
+	    + "?coinflip *amount* @person - coinflip between you and the person you tagged for a specific amount of money.\r\n"
+	    + "\r\n" + "?dailyspin - daily spin which will be available once every day for free.\r\n" + "\r\n"
+	    + "?dailyspin buy - after using your daily spin you will be able to buy another daily spin for 75 coins by using this command.\r\n"
+	    + "\r\n" + "?debt - display your debt\r\n" + "\r\n"
+	    + "?give *amount* @person - will give the person you tag a specific amount of money.\r\n" + "\r\n"
 	    + "?help - display this message.\r\n" + "\r\n"
 	    + "?leaderboard - print the top 5 richest members of the server\r\n" + "\r\n"
+	    + "?loan *amount* - take a loan for up to (500 + 3 * balance). You will return 5% extra. 10% of your income is taken until you return your loan\r\n"
+	    + "\r\n" + "?overview - display your balance and your debt\r\n" + "\r\n"
+	    + "?paytodebt *amount* - pay given amount to fill your debt, excess money will be returned to your balance.\r\n"
+	    + "\r\n"
 	    + "?rps *amount* @person - rock paper scissors. You will be sent a message from the bot and you will need to react with one of the three options.\r\n"
 	    + "\r\n"
-	    + "?rt *amount* *color* (red, black or green) - roulette. can choose from red(x2), black(x2) and green(x14). If you win your money will be multiplied by the numbers next to the color.\r\n"
-	    + "\r\n" + "alternative ways of writing the commands:\r\n" + "?bal = ?balance, ?cash, ?money\r\n"
-	    + "?cf = ?coinflip\r\n" + "?ds = ?dailyspin\r\n" + "?rt = ?roulette\r\n" + "?give = ?transfer```";
+	    + "?roulette *amount* *color* (red, black or green) - roulette. can choose from red(x2), black(x2) and green(x14). If you win your money will be multiplied by the numbers next to the color.\r\n"
+	    + "\r\n" + "alternative ways of writing the commands:\r\n" + "?ballance = ?bal, ?cash, ?money\r\n"
+	    + "?coinflip = ?cf\r\n" + "?dailyspin = ?ds\r\n" + "?leaderboard = ?lb\r\n" + "?overview = ?ov\r\n"
+	    + "?paytodebt = ?ptd\r\n" + "?roulette = ?rt\r\n" + "?give = ?transfer```";
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
